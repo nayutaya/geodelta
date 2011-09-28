43640cccca89550808422a36c4f9533e3b3f3523
Yuya Kato <yuyakato@gmail.com>
2011-09-28 11:16:26 +0900

    ワールドデルタをエンコードする処理を追加

--- src/ruby/lib/geodelta/encoder.rb
+++ src/ruby/lib/geodelta/encoder.rb
@@ -2,5 +2,20 @@
 
 module GeoDelta
   module Encoder
+    WORLD_DELTA_TABLE = [
+      [0, "Z"],
+      [1, "Y"],
+      [2, "X"],
+      [3, "W"],
+      [4, "V"],
+      [5, "T"],
+      [6, "S"],
+      [7, "R"],
+    ].each(&:freeze).freeze
+    WORLD_ID_TO_CHAR = WORLD_DELTA_TABLE.inject({}) { |memo, (num, char)| memo[num] = char; memo }.freeze
+
+    def self.encode_world_delta(id)
+      return WORLD_ID_TO_CHAR[id] || raise("invalid world delta id -- #{id}")
+    end
   end
 end
--- src/ruby/test/encoder_test.rb
+++ src/ruby/test/encoder_test.rb
@@ -7,4 +7,17 @@ class GeoDeltaEncoderTest < Test::Unit::TestCase
   def setup
     @mod = GeoDelta::Encoder
   end
+
+  def test_encode_world_delta
+    assert_equal("Z", @mod.encode_world_delta(0))
+    assert_equal("Y", @mod.encode_world_delta(1))
+    assert_equal("X", @mod.encode_world_delta(2))
+    assert_equal("W", @mod.encode_world_delta(3))
+    assert_equal("V", @mod.encode_world_delta(4))
+    assert_equal("T", @mod.encode_world_delta(5))
+    assert_equal("S", @mod.encode_world_delta(6))
+    assert_equal("R", @mod.encode_world_delta(7))
+    assert_raise(RuntimeError) { @mod.encode_world_delta(-1) }
+    assert_raise(RuntimeError) { @mod.encode_world_delta(8) }
+  end
 end
