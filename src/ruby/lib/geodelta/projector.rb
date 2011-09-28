e90dd2e915c47c65723eef3b970a1e7422d1dc82
Yuya Kato <yuyakato@gmail.com>
2011-09-28 11:08:10 +0900

    緯度経度をメルカトルXY座標に変換する処理を追加

--- src/ruby/lib/geodelta/projector.rb
+++ src/ruby/lib/geodelta/projector.rb
@@ -2,5 +2,20 @@
 
 module GeoDelta
   module Projector
+    DEG2RAD      = Math::PI / 180.0 # 度をラジアンに変換するための係数
+
+    # 緯度をメルカトルY座標に変換する
+    #   -90.0 <= lat <= +90.0
+    #    -1.0 <= my  <=  +1.0
+    def self.lat_to_my(lat)
+      return Math.atanh(Math.sin(lat * DEG2RAD)) / Math::PI
+    end
+
+    # 経度をメルカトルX座標に変換する
+    #   -180.0 <= lng <= +180.0
+    #     -1.0 <= mx  <=   +1.0
+    def self.lng_to_mx(lng)
+      return lng / 180.0
+    end
   end
 end
--- src/ruby/test/projector_test.rb
+++ src/ruby/test/projector_test.rb
@@ -7,4 +7,18 @@ class GeoDeltaProjectorTest < Test::Unit::TestCase
   def setup
     @mod = GeoDelta::Projector
   end
+
+  def test_lat_to_my
+    assert_in_delta(+1.0, @mod.lat_to_my(+85.0511), 1.0E-5)
+    assert_equal(0.0, @mod.lat_to_my(0.0))
+    assert_in_delta(-1.0, @mod.lat_to_my(-85.0511), 1.0E-5)
+  end
+
+  def test_lng_to_mx
+    assert_equal(+1.0, @mod.lng_to_mx(+180.0))
+    assert_equal(+0.5, @mod.lng_to_mx( +90.0))
+    assert_equal( 0.0, @mod.lng_to_mx(   0.0))
+    assert_equal(-0.5, @mod.lng_to_mx( -90.0))
+    assert_equal(-1.0, @mod.lng_to_mx(-180.0))
+  end
 end
