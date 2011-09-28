25d74f458d2c354d6423647e5fe022414e56e08f
Yuya Kato <yuyakato@gmail.com>
2011-09-28 11:26:31 +0900

    XY座標からワールドデルタIDを取得する処理を追加

--- src/ruby/lib/geodelta/geometry.rb
+++ src/ruby/lib/geodelta/geometry.rb
@@ -2,5 +2,19 @@
 
 module GeoDelta
   module Geometry
+    # 指定された座標(x,y)に該当するワールドデルタの番号を返す
+    # ただし、-12.0 <= x <= +12.0、-12.0 <= y <= +12.0
+    def self.get_world_delta_id(x, y)
+      xx = x % 24
+      yy = y.abs
+      return (y >= 0.0 ? 0 : 4) +
+        case
+        when yy >= +2.0 * (xx -  0.0) then 0
+        when yy <= -2.0 * (xx - 12.0) then 1
+        when yy >= +2.0 * (xx - 12.0) then 2
+        when yy <= -2.0 * (xx - 24.0) then 3
+        else                               0
+        end
+    end
   end
 end
--- src/ruby/test/geometry_test.rb
+++ src/ruby/test/geometry_test.rb
@@ -7,4 +7,48 @@ class GeoDeltaGeometryTest < Test::Unit::TestCase
   def setup
     @mod = GeoDelta::Geometry
   end
+
+  def test_get_world_delta_id
+    assert_equal(2, @mod.get_world_delta_id( -6.0, +12.0))
+    assert_equal(3, @mod.get_world_delta_id( -6.0,  +6.0))
+    assert_equal(3, @mod.get_world_delta_id( -6.0,   0.0))
+    assert_equal(3, @mod.get_world_delta_id( -3.0,  +6.0))
+    assert_equal(0, @mod.get_world_delta_id(  0.0, +12.0))
+    assert_equal(0, @mod.get_world_delta_id(  0.0,  +6.0))
+    assert_equal(0, @mod.get_world_delta_id(  0.0,   0.0))
+    assert_equal(0, @mod.get_world_delta_id( +3.0,  +6.0))
+    assert_equal(0, @mod.get_world_delta_id( +6.0, +12.0))
+    assert_equal(1, @mod.get_world_delta_id( +6.0,  +6.0))
+    assert_equal(1, @mod.get_world_delta_id( +6.0,   0.0))
+    assert_equal(1, @mod.get_world_delta_id( +9.0,  +6.0))
+    assert_equal(1, @mod.get_world_delta_id(+12.0,   0.0))
+    assert_equal(2, @mod.get_world_delta_id(+12.0, +12.0))
+    assert_equal(2, @mod.get_world_delta_id(+12.0,  +6.0))
+    assert_equal(2, @mod.get_world_delta_id(+15.0,  +6.0))
+    assert_equal(2, @mod.get_world_delta_id(+18.0, +12.0))
+    assert_equal(3, @mod.get_world_delta_id(+18.0,  +6.0))
+    assert_equal(3, @mod.get_world_delta_id(+18.0,   0.0))
+    assert_equal(3, @mod.get_world_delta_id(+21.0,  +6.0))
+    assert_equal(0, @mod.get_world_delta_id(+24.0, +12.0))
+    assert_equal(0, @mod.get_world_delta_id(+24.0,  +6.0))
+    assert_equal(0, @mod.get_world_delta_id(+24.0,   0.0))
+
+    assert_equal(6, @mod.get_world_delta_id( -6.0, -12.0))
+    assert_equal(7, @mod.get_world_delta_id( -6.0,  -6.0))
+    assert_equal(7, @mod.get_world_delta_id( -3.0,  -6.0))
+    assert_equal(4, @mod.get_world_delta_id(  0.0,  -6.0))
+    assert_equal(4, @mod.get_world_delta_id(  0.0, -12.0))
+    assert_equal(4, @mod.get_world_delta_id( +3.0,  -6.0))
+    assert_equal(4, @mod.get_world_delta_id( +6.0, -12.0))
+    assert_equal(5, @mod.get_world_delta_id( +6.0,  -6.0))
+    assert_equal(5, @mod.get_world_delta_id( +9.0,  -6.0))
+    assert_equal(6, @mod.get_world_delta_id(+12.0,  -6.0))
+    assert_equal(6, @mod.get_world_delta_id(+12.0, -12.0))
+    assert_equal(6, @mod.get_world_delta_id(+15.0,  -6.0))
+    assert_equal(6, @mod.get_world_delta_id(+18.0, -12.0))
+    assert_equal(7, @mod.get_world_delta_id(+18.0,  -6.0))
+    assert_equal(7, @mod.get_world_delta_id(+21.0,  -6.0))
+    assert_equal(4, @mod.get_world_delta_id(+24.0,  -6.0))
+    assert_equal(4, @mod.get_world_delta_id(+24.0, -12.0))
+  end
 end
