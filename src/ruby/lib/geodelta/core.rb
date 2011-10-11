001c078f8b1e3d3e09c71154248b153c6ccedd87
Yuya Kato <yuyakato@gmail.com>
2011-10-11 19:45:46 +0900

    緯度経度からデルタID列を取得する処理を追加

--- src/ruby/lib/geodelta/core.rb
+++ src/ruby/lib/geodelta/core.rb
@@ -1,4 +1,15 @@
 # encoding: utf-8
 
+require File.expand_path(File.join(File.dirname(__FILE__), "projector"))
+require File.expand_path(File.join(File.dirname(__FILE__), "geometry"))
+
 module GeoDelta
+  def self.get_delta_ids(lat, lng, level)
+    mx  = GeoDelta::Projector.lng_to_mx(lng)
+    my  = GeoDelta::Projector.lat_to_my(lat)
+    nx  = GeoDelta::Projector.mx_to_nx(mx)
+    ny  = GeoDelta::Projector.my_to_ny(my)
+    ids = GeoDelta::Geometry.get_delta_ids(nx, ny, level)
+    return ids
+  end
 end
--- src/ruby/test/core_test.rb
+++ src/ruby/test/core_test.rb
@@ -7,4 +7,23 @@ class GeoDeltaTest < Test::Unit::TestCase
   def setup
     @mod = GeoDelta
   end
+
+  def test_get_delta_ids
+    assert_equal([0], @mod.get_delta_ids(+45.0,   +0.0, 1))
+    assert_equal([1], @mod.get_delta_ids(+45.0,  +90.0, 1))
+    assert_equal([2], @mod.get_delta_ids(+45.0, +180.0, 1))
+    assert_equal([3], @mod.get_delta_ids(+45.0,  -90.0, 1))
+    assert_equal([2], @mod.get_delta_ids(+45.0, -180.0, 1))
+
+    assert_equal([4], @mod.get_delta_ids(-45.0,   +0.0, 1))
+    assert_equal([5], @mod.get_delta_ids(-45.0,  +90.0, 1))
+    assert_equal([6], @mod.get_delta_ids(-45.0, +180.0, 1))
+    assert_equal([7], @mod.get_delta_ids(-45.0,  -90.0, 1))
+    assert_equal([6], @mod.get_delta_ids(-45.0, -180.0, 1))
+
+    assert_equal([0],          @mod.get_delta_ids(+0.0, +0.0, 1))
+    assert_equal([0, 1],       @mod.get_delta_ids(+0.0, +0.0, 2))
+    assert_equal([0, 1, 1],    @mod.get_delta_ids(+0.0, +0.0, 3))
+    assert_equal([0, 1, 1, 1], @mod.get_delta_ids(+0.0, +0.0, 4))
+  end
 end
