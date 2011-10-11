d6032cf14da152ef59f1fabaadfeb48ea60eac11
Yuya Kato <yuyakato@gmail.com>
2011-10-11 19:46:35 +0900

    緯度経度からGeoDeltaコードを取得する処理を追加

--- src/ruby/lib/geodelta/core.rb
+++ src/ruby/lib/geodelta/core.rb
@@ -2,6 +2,7 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), "projector"))
 require File.expand_path(File.join(File.dirname(__FILE__), "geometry"))
+require File.expand_path(File.join(File.dirname(__FILE__), "encoder"))
 
 module GeoDelta
   def self.get_delta_ids(lat, lng, level)
@@ -11,5 +12,11 @@ module GeoDelta
     ny  = GeoDelta::Projector.my_to_ny(my)
     ids = GeoDelta::Geometry.get_delta_ids(nx, ny, level)
     return ids
+  end
+
+  def self.get_delta_code(lat, lng, level)
+    ids  = self.get_delta_ids(lat, lng, level)
+    code = GeoDelta::Encoder.encode(ids)
+    return code
   end
 end
--- src/ruby/test/core_test.rb
+++ src/ruby/test/core_test.rb
@@ -26,4 +26,23 @@ class GeoDeltaTest < Test::Unit::TestCase
     assert_equal([0, 1, 1],    @mod.get_delta_ids(+0.0, +0.0, 3))
     assert_equal([0, 1, 1, 1], @mod.get_delta_ids(+0.0, +0.0, 4))
   end
+
+  def test_get_delta_code
+    assert_equal("Z", @mod.get_delta_code(+45.0,   +0.0, 1))
+    assert_equal("Y", @mod.get_delta_code(+45.0,  +90.0, 1))
+    assert_equal("X", @mod.get_delta_code(+45.0, +180.0, 1))
+    assert_equal("W", @mod.get_delta_code(+45.0,  -90.0, 1))
+    assert_equal("X", @mod.get_delta_code(+45.0, -180.0, 1))
+
+    assert_equal("V", @mod.get_delta_code(-45.0,   +0.0, 1))
+    assert_equal("T", @mod.get_delta_code(-45.0,  +90.0, 1))
+    assert_equal("S", @mod.get_delta_code(-45.0, +180.0, 1))
+    assert_equal("R", @mod.get_delta_code(-45.0,  -90.0, 1))
+    assert_equal("S", @mod.get_delta_code(-45.0, -180.0, 1))
+
+    assert_equal("Z",   @mod.get_delta_code(+0.0, +0.0, 1))
+    assert_equal("ZM",  @mod.get_delta_code(+0.0, +0.0, 2))
+    assert_equal("Z7",  @mod.get_delta_code(+0.0, +0.0, 3))
+    assert_equal("Z7M", @mod.get_delta_code(+0.0, +0.0, 4))
+  end
 end
