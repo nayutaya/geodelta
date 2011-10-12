ff9cb63db26e5c292180b810fe00864fa881970b
Yuya Kato <yuyakato@gmail.com>
2011-10-13 01:53:57 +0900

    エンコード処理を追加

--- src/ruby/example/geodelta_server/config.ru
+++ src/ruby/example/geodelta_server/config.ru
@@ -1,6 +1,5 @@
 # encoding: utf-8
 
-require "sinatra"
 require "./server"
 
-run Sinatra::Application
+run GeoDeltaServer.new
--- src/ruby/example/geodelta_server/server.rb
+++ src/ruby/example/geodelta_server/server.rb
@@ -1,10 +1,32 @@
 # encoding: utf-8
 
-require "sinatra"
+require "sinatra/base"
+require "json"
 
-get "/api/encode" do
-  lat   = (params["lat"] || "0.0").to_f
-  lng   = (params["lng"] || "0.0").to_f
-  level = (params["level"] || "1").to_i
-  "encode lat=#{lat} lng=#{lng} level=#{level}"
+require_relative "../../lib/geodelta"
+
+class GeoDeltaServer < Sinatra::Base
+  get "/api/encode" do
+    lat   = (params["lat"]   || "0.0").to_f
+    lng   = (params["lng"]   || "0.0").to_f
+    level = (params["level"] || "1"  ).to_i
+
+    code       = GeoDelta.get_delta_code(lat, lng, level)
+    clat, clng = GeoDelta.get_center_latlng_from_delta_code(code)
+
+    response = {
+      "request" => {
+        "lat"   => lat,
+        "lng"   => lng,
+        "level" => level,
+      },
+      "response" => {
+        "code"   => code,
+        "center" => {"lat" => clat, "lng" => clng},
+      },
+    }
+
+    content_type(:json)
+    return JSON.dump(response)
+  end
 end
