3476badf734584746f91d6099fab1b3e0e71a05c
Yuya Kato <yuyakato@gmail.com>
2011-10-19 02:27:46 +0900

    デルタのポリラインを描画する処理を追加

--- src/ruby/example/geodelta_server/public/app1.js
+++ src/ruby/example/geodelta_server/public/app1.js
@@ -15,11 +15,27 @@ $(function() {
       level: 10
     }
     $.get("/api/encode", params, function(data) {
+/*
       var center = new google.maps.LatLng(data.response.coordinates[0].lat, data.response.coordinates[0].lng);
       var marker = new google.maps.Marker({
-        position: center,
         map: map, 
+        position: center,
         title: "code=" + data.response.code
+      });
+*/
+      var coordinates = data.response.coordinates;
+      var points = [
+        new google.maps.LatLng(coordinates[1].lat, coordinates[1].lng),
+        new google.maps.LatLng(coordinates[2].lat, coordinates[2].lng),
+        new google.maps.LatLng(coordinates[3].lat, coordinates[3].lng),
+        new google.maps.LatLng(coordinates[1].lat, coordinates[1].lng)
+      ];
+      var path = new google.maps.Polyline({
+        map: map, 
+        path: points,
+        strokeColor: "#FF0000",
+        strokeOpacity: 0.5,
+        strokeWeight: 2
       });
     });
   });
