6222b01dfe54cbb7b0a1b5a6aa66305e1cce8ce4
Yuya Kato <yuyakato@gmail.com>
2011-10-25 11:45:25 +0900

    Utilityクラスを追加

---
+++ src/java/geodelta-test/src/jp/nayutaya/geodelta/UtilityTest.java
@@ -0,0 +1,32 @@
+
+package jp.nayutaya.geodelta;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class UtilityTest
+{
+    @Test
+    public void mod()
+    {
+        assertEquals(0.0, Utility.mod(+0.0, 1.0), 1.0E-15);
+        assertEquals(0.0, Utility.mod(+1.0, 1.0), 1.0E-15);
+        assertEquals(0.5, Utility.mod(+1.5, 1.0), 1.0E-15);
+        assertEquals(0.0, Utility.mod(+2.0, 1.0), 1.0E-15);
+
+        assertEquals(0.0, Utility.mod(+0.0, 2.0), 1.0E-15);
+        assertEquals(1.0, Utility.mod(+1.0, 2.0), 1.0E-15);
+        assertEquals(0.0, Utility.mod(+2.0, 2.0), 1.0E-15);
+        assertEquals(1.0, Utility.mod(+3.0, 2.0), 1.0E-15);
+        assertEquals(0.0, Utility.mod(+4.0, 2.0), 1.0E-15);
+
+        assertEquals(0.0, Utility.mod(-1.0, 1.0), 1.0E-15);
+        assertEquals(0.5, Utility.mod(-1.5, 1.0), 1.0E-15);
+        assertEquals(0.0, Utility.mod(-2.0, 1.0), 1.0E-15);
+
+        assertEquals(1.0, Utility.mod(-1.0, 2.0), 1.0E-15);
+        assertEquals(0.0, Utility.mod(-2.0, 2.0), 1.0E-15);
+        assertEquals(1.0, Utility.mod(-3.0, 2.0), 1.0E-15);
+        assertEquals(0.0, Utility.mod(-4.0, 2.0), 1.0E-15);
+    }
+}
