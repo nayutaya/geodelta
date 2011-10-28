7e06d1e02e19a885a0016cb4fc6f58732326130a
Yuya Kato <yuyakato@gmail.com>
2011-10-29 01:46:48 +0900

    32ビット整数にGeoDeltaコードをパックする処理を追加

--- src/ruby/lib/geodelta/packer32.rb
+++ src/ruby/lib/geodelta/packer32.rb
@@ -2,5 +2,42 @@
 
 module GeoDelta
   class Packer32
+    def pack_world_delta(id)
+      return id << 28
+    end
+
+    def unpack_world_delta(value)
+      return (value >> 28) & 0b111
+    end
+
+    def pack_sub_delta(level, id)
+      return id << (26 - ((level - 2) * 2))
+    end
+
+    def unpack_sub_delta(level, value)
+      return (value >> (26 - ((level - 2) * 2))) & 0b11
+    end
+
+    def pack_level(level)
+      return level
+    end
+
+    def unpack_level(value)
+      return value & 0b1111
+    end
+
+    def pack(ids)
+      wid   = self.pack_world_delta(ids[0])
+      sids  = ids[1..-1].each_with_index.map { |id, i| self.pack_sub_delta(i + 2, id) }.inject(0, &:+)
+      level = self.pack_level(ids.size)
+      return wid + sids + level
+    end
+
+    def unpack(value)
+      level = self.unpack_level(value)
+      wid   = self.unpack_world_delta(value)
+      sids  = (level - 1).times.map { |i| self.unpack_sub_delta(i + 2, value) }
+      return [wid] + sids
+    end
   end
 end
--- src/ruby/test/packer32_test.rb
+++ src/ruby/test/packer32_test.rb
@@ -5,6 +5,130 @@ require_relative "../lib/geodelta/packer32"
 
 class GeoDeltaPacker32Test < Test::Unit::TestCase
   def setup
-    @mod = GeoDelta::Packer32
+    @packer = GeoDelta::Packer32.new
+  end
+
+  def test_pack_world_delta
+    ############ 0bXWwwSsSsSsSsSsSsSsSsSsSsSsSsLlll
+    assert_equal(0b00000000000000000000000000000000, @packer.pack_world_delta(0))
+    assert_equal(0b00010000000000000000000000000000, @packer.pack_world_delta(1))
+    assert_equal(0b00100000000000000000000000000000, @packer.pack_world_delta(2))
+    assert_equal(0b00110000000000000000000000000000, @packer.pack_world_delta(3))
+    assert_equal(0b01000000000000000000000000000000, @packer.pack_world_delta(4))
+    assert_equal(0b01010000000000000000000000000000, @packer.pack_world_delta(5))
+    assert_equal(0b01100000000000000000000000000000, @packer.pack_world_delta(6))
+    assert_equal(0b01110000000000000000000000000000, @packer.pack_world_delta(7))
+  end
+
+  def test_unpack_world_delta
+    ########################################## 0bXWwwSsSsSsSsSsSsSsSsSsSsSsSsLlll
+    assert_equal(0, @packer.unpack_world_delta(0b00000000000000000000000000000000))
+    assert_equal(1, @packer.unpack_world_delta(0b00010000000000000000000000000000))
+    assert_equal(2, @packer.unpack_world_delta(0b00100000000000000000000000000000))
+    assert_equal(3, @packer.unpack_world_delta(0b00110000000000000000000000000000))
+    assert_equal(4, @packer.unpack_world_delta(0b01000000000000000000000000000000))
+    assert_equal(5, @packer.unpack_world_delta(0b01010000000000000000000000000000))
+    assert_equal(6, @packer.unpack_world_delta(0b01100000000000000000000000000000))
+    assert_equal(7, @packer.unpack_world_delta(0b01110000000000000000000000000000))
+  end
+
+  def test_pack_sub_delta
+    ############ 0bXWwwSsSsSsSsSsSsSsSsSsSsSsSsLlll
+    assert_equal(0b00000000000000000000000000000000, @packer.pack_sub_delta( 2, 0))
+    assert_equal(0b00000100000000000000000000000000, @packer.pack_sub_delta( 2, 1))
+    assert_equal(0b00001000000000000000000000000000, @packer.pack_sub_delta( 2, 2))
+    assert_equal(0b00001100000000000000000000000000, @packer.pack_sub_delta( 2, 3))
+    assert_equal(0b00000000000000000000000000000000, @packer.pack_sub_delta( 3, 0))
+    assert_equal(0b00000001000000000000000000000000, @packer.pack_sub_delta( 3, 1))
+    assert_equal(0b00000010000000000000000000000000, @packer.pack_sub_delta( 3, 2))
+    assert_equal(0b00000011000000000000000000000000, @packer.pack_sub_delta( 3, 3))
+
+    ############ 0bXWwwSsSsSsSsSsSsSsSsSsSsSsSsLlll
+    assert_equal(0b00000000110000000000000000000000, @packer.pack_sub_delta( 4, 3))
+    assert_equal(0b00000000001100000000000000000000, @packer.pack_sub_delta( 5, 3))
+    assert_equal(0b00000000000011000000000000000000, @packer.pack_sub_delta( 6, 3))
+    assert_equal(0b00000000000000110000000000000000, @packer.pack_sub_delta( 7, 3))
+    assert_equal(0b00000000000000001100000000000000, @packer.pack_sub_delta( 8, 3))
+    assert_equal(0b00000000000000000011000000000000, @packer.pack_sub_delta( 9, 3))
+    assert_equal(0b00000000000000000000110000000000, @packer.pack_sub_delta(10, 3))
+    assert_equal(0b00000000000000000000001100000000, @packer.pack_sub_delta(11, 3))
+    assert_equal(0b00000000000000000000000011000000, @packer.pack_sub_delta(12, 3))
+    assert_equal(0b00000000000000000000000000110000, @packer.pack_sub_delta(13, 3))
+  end
+
+  def test_unpack_sub_delta
+    ############################################ 0bXWwwSsSsSsSsSsSsSsSsSsSsSsSsLlll
+    assert_equal(0, @packer.unpack_sub_delta( 2, 0b00000000000000000000000000000000))
+    assert_equal(1, @packer.unpack_sub_delta( 2, 0b00000100000000000000000000000000))
+    assert_equal(2, @packer.unpack_sub_delta( 2, 0b00001000000000000000000000000000))
+    assert_equal(3, @packer.unpack_sub_delta( 2, 0b00001100000000000000000000000000))
+    assert_equal(0, @packer.unpack_sub_delta( 3, 0b00000000000000000000000000000000))
+    assert_equal(1, @packer.unpack_sub_delta( 3, 0b00000001000000000000000000000000))
+    assert_equal(2, @packer.unpack_sub_delta( 3, 0b00000010000000000000000000000000))
+    assert_equal(3, @packer.unpack_sub_delta( 3, 0b00000011000000000000000000000000))
+
+    ############################################ 0bXWwwSsSsSsSsSsSsSsSsSsSsSsSsLlll
+    assert_equal(3, @packer.unpack_sub_delta( 4, 0b00000000110000000000000000000000))
+    assert_equal(3, @packer.unpack_sub_delta( 5, 0b00000000001100000000000000000000))
+    assert_equal(3, @packer.unpack_sub_delta( 6, 0b00000000000011000000000000000000))
+    assert_equal(3, @packer.unpack_sub_delta( 7, 0b00000000000000110000000000000000))
+    assert_equal(3, @packer.unpack_sub_delta( 8, 0b00000000000000001100000000000000))
+    assert_equal(3, @packer.unpack_sub_delta( 9, 0b00000000000000000011000000000000))
+    assert_equal(3, @packer.unpack_sub_delta(10, 0b00000000000000000000110000000000))
+    assert_equal(3, @packer.unpack_sub_delta(11, 0b00000000000000000000001100000000))
+    assert_equal(3, @packer.unpack_sub_delta(12, 0b00000000000000000000000011000000))
+    assert_equal(3, @packer.unpack_sub_delta(13, 0b00000000000000000000000000110000))
+  end
+
+  def test_pack_level
+    ############ 0bXWwwSsSsSsSsSsSsSsSsSsSsSsSsLlll
+    assert_equal(0b00000000000000000000000000000001, @packer.pack_level( 1))
+    assert_equal(0b00000000000000000000000000000010, @packer.pack_level( 2))
+    assert_equal(0b00000000000000000000000000001100, @packer.pack_level(12))
+    assert_equal(0b00000000000000000000000000001101, @packer.pack_level(13))
+  end
+
+  def test_unpack_level
+    ##################################### 0bXWwwSsSsSsSsSsSsSsSsSsSsSsSsLlll
+    assert_equal( 1, @packer.unpack_level(0b00000000000000000000000000000001))
+    assert_equal( 2, @packer.unpack_level(0b00000000000000000000000000000010))
+    assert_equal(12, @packer.unpack_level(0b00000000000000000000000000001100))
+    assert_equal(13, @packer.unpack_level(0b00000000000000000000000000001101))
+  end
+
+  def test_pack
+    ############ 0bXWwwSsSsSsSsSsSsSsSsSsSsSsSsLlll
+    assert_equal(0b00000000000000000000000000000001, @packer.pack([0]))
+    assert_equal(0b01110000000000000000000000000001, @packer.pack([7]))
+    assert_equal(0b00000100000000000000000000000010, @packer.pack([0, 1]))
+    assert_equal(0b00101100000000000000000000000010, @packer.pack([2, 3]))
+    assert_equal(0b00000000000000000000000000001101, @packer.pack([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]))
+    assert_equal(0b00010101010101010101010101011101, @packer.pack([1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]))
+    assert_equal(0b01111111111111111111111111111101, @packer.pack([7, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3]))
+  end
+
+  def test_unpack
+    ################ 0bXWwwSsSsSsSsSsSsSsSsSsSsSsSsLlll
+    assert_equal(
+      [0],
+      @packer.unpack(0b00000000000000000000000000000001))
+    assert_equal(
+      [7],
+      @packer.unpack(0b01110000000000000000000000000001))
+    assert_equal(
+      [0, 1],
+      @packer.unpack(0b00000100000000000000000000000010))
+    assert_equal(
+      [2, 3],
+      @packer.unpack(0b00101100000000000000000000000010))
+    assert_equal(
+      [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+      @packer.unpack(0b00000000000000000000000000001101))
+    assert_equal(
+      [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
+      @packer.unpack(0b00010101010101010101010101011101))
+    assert_equal(
+      [7, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3],
+      @packer.unpack(0b01111111111111111111111111111101))
   end
 end
