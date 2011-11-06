32b75607b34e9adc450b4fe05e663da5d7f3e6a3
Yuya Kato <yuyakato@gmail.com>
2011-11-07 01:54:51 +0900

    実験用プログラムのひな形を追加

---
+++ src/ruby/random_region_level2.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+# TODO: random_region_level2.svgを出力する
+# TODO: レベル2のすべてのデルタを出力する
+# TODO: ランダムな矩形を求める
+# TODO: ランダムな矩形と交差するデルタを求める
+# TODO: ランダムな矩形と交差するデルタを出力する
+# TODO: ランダムな矩形を出力する
