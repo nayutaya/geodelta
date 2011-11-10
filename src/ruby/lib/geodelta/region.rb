53ec8ccfcc0feb041dd1bfc0dbf238142b66077f
Yuya Kato <yuyakato@gmail.com>
2011-11-10 12:15:25 +0900

    実験コードを追加

--- src/ruby/lib/geodelta/region.rb
+++ src/ruby/lib/geodelta/region.rb
@@ -5,6 +5,44 @@ require_relative "geometry"
 module GeoDelta
   module Region
     def self.get_delta_ids_in_region(x1, y1, x2, y2, level)
+=begin
+      ids = []
+      ids << (nw = GeoDelta::Geometry.get_delta_ids(x1, y1, level))
+      ids << (ne = GeoDelta::Geometry.get_delta_ids(x2, y1, level))
+      ids << (sw = GeoDelta::Geometry.get_delta_ids(x1, y2, level))
+      ids << (se = GeoDelta::Geometry.get_delta_ids(x2, y2, level))
+=end
+
+# 上辺の一列
+=begin
+      sx, y = GeoDelta::Geometry.get_center(nw)
+      ex, _ = GeoDelta::Geometry.get_center(ne)
+
+      unit = 12.0 / (2 ** (level - 1))
+      y += (unit / 6) * (GeoDelta::Geometry.upper_delta?(nw) ? +1 : -1)
+      dx = ex - sx
+
+      (0..((dx / (unit / 2)).floor)).each { |i|
+        xx = sx + ((unit / 2) * i)
+        ids << GeoDelta::Geometry.get_delta_ids(xx, y, level)
+      }
+=end
+
+# 下辺の一列
+=begin
+      sx, y = GeoDelta::Geometry.get_center(sw)
+      ex, _ = GeoDelta::Geometry.get_center(se)
+
+      unit = 12.0 / (2 ** (level - 1))
+      y += (unit / 6) * (GeoDelta::Geometry.upper_delta?(nw) ? +1 : -1)
+      dx = ex - sx
+
+      (0..((dx / (unit / 2)).floor)).each { |i|
+        xx = sx + ((unit / 2) * i)
+        ids << GeoDelta::Geometry.get_delta_ids(xx, y, level)
+      }
+=end
+
       s_ids  = GeoDelta::Geometry.get_delta_ids(x1, y1, level)
       e_ids  = GeoDelta::Geometry.get_delta_ids(x2, y2, level)
       sx, sy = GeoDelta::Geometry.get_center(s_ids)
