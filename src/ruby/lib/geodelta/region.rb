658ab674e4259f029677eccdaa1ad7a22b3e414d
Yuya Kato <yuyakato@gmail.com>
2011-11-14 01:24:10 +0900

    矩形に含まれるデルタを取得する処理を修正

--- src/ruby/lib/geodelta/region.rb
+++ src/ruby/lib/geodelta/region.rb
@@ -7,24 +7,45 @@ module GeoDelta
     def self.get_delta_ids_in_region(x1, y1, x2, y2, level)
       ids = []
       unit = 12.0 / (2 ** (level - 1))
+      u1   = unit
+      u2   = unit / 2
 
-      ids << (nw = GeoDelta::Geometry.get_delta_ids(x1, y1, level))
-      ids << (ne = GeoDelta::Geometry.get_delta_ids(x2, y1, level))
-      ids << (sw = GeoDelta::Geometry.get_delta_ids(x1, y2, level))
-      ids << (se = GeoDelta::Geometry.get_delta_ids(x2, y2, level))
+#      ids << (nw = GeoDelta::Geometry.get_delta_ids(x1, y1, level))
+#      ids << (ne = GeoDelta::Geometry.get_delta_ids(x2, y1, level))
+#      ids << (sw = GeoDelta::Geometry.get_delta_ids(x1, y2, level))
+#      ids << (se = GeoDelta::Geometry.get_delta_ids(x2, y2, level))
+      (nw = GeoDelta::Geometry.get_delta_ids(x1, y1, level))
+      (ne = GeoDelta::Geometry.get_delta_ids(x2, y1, level))
+      (sw = GeoDelta::Geometry.get_delta_ids(x1, y2, level))
+      (se = GeoDelta::Geometry.get_delta_ids(x2, y2, level))
 
       # 上辺の一列
-      sx, y = GeoDelta::Geometry.get_center(nw)
-      ex, _ = GeoDelta::Geometry.get_center(ne)
+      proc {
+        sx, y = GeoDelta::Geometry.get_center(nw)
+        ex, _ = GeoDelta::Geometry.get_center(ne)
 
-      y += (unit / 6) * (GeoDelta::Geometry.upper_delta?(nw) ? +1 : -1)
-      dx = ex - sx
+        sxi = (sx / u2).floor
+        exi = (ex / u2).ceil
 
-      (0..((dx / (unit / 2)).floor)).each { |i|
-        xx = sx + ((unit / 2) * i)
-        ids << GeoDelta::Geometry.get_delta_ids(xx, y, level)
-      }
+        if !GeoDelta::Geometry.upper_delta?(nw)
+          if x1 < sx && nw != sw
+            sxi -= 1
+          end
+        end
 
+        if !GeoDelta::Geometry.upper_delta?(ne)
+          if x2 > ex && ne != se
+            exi += 1
+          end
+        end
+
+        (sxi..exi).each { |xi|
+          xx = xi * u2
+          ids << GeoDelta::Geometry.get_delta_ids(xx, y, level)
+        }
+      }.call
+
+=begin
       # 下辺の一列
       sx, y = GeoDelta::Geometry.get_center(sw)
       ex, _ = GeoDelta::Geometry.get_center(se)
@@ -48,6 +69,7 @@ module GeoDelta
           ids << GeoDelta::Geometry.get_delta_ids(xx, yy, level)
         }
       }
+=end
 
 =begin
       s_ids  = GeoDelta::Geometry.get_delta_ids(x1, y1, level)
