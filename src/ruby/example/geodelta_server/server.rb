eb1d08a90bad92ce9c0f1f33fdd47f83c5200e45
Yuya Kato <yuyakato@gmail.com>
2011-11-15 00:25:57 +0900

    「/demo/random_region_delta_level2.svg」を追加

--- src/ruby/example/geodelta_server/server.rb
+++ src/ruby/example/geodelta_server/server.rb
@@ -2,8 +2,12 @@
 
 require "sinatra/base"
 require "json"
+require "stringio"
 
 require_relative "../../lib/geodelta"
+require_relative "../../lib/geodelta/region"
+require_relative "../../lib/geodelta/id_util"
+require_relative "../svg"
 
 class GeoDeltaServer < Sinatra::Base
   set :public_folder, File.dirname(__FILE__) + "/public"
@@ -32,5 +36,73 @@ class GeoDeltaServer < Sinatra::Base
 
     content_type(:json)
     return JSON.dump(response)
+  end
+
+  get "/demo/random_region_delta_level2.svg" do
+    level = 2
+    x1, x2 = [(rand * 24) - 12, (rand * 24) - 12].sort
+    y1, y2 = [(rand * 24) - 12, (rand * 24) - 12].sort.reverse
+    dx = x2 - x1
+    dy = y1 - y2
+
+    svg = SVG.new(
+      "width"   => "190mm",
+      "height"  => "190mm",
+      "viewBox" => "-16.0 -14.0 32.0 28.0")
+
+    svg.style(
+      "polygon.a",
+      "fill"   => "green",
+      "stroke" => "none")
+
+    svg.style(
+      "polygon.b",
+      "fill"         => "none",
+      "stroke"       => "black",
+      "stroke-width" => "0.05")
+
+    svg.style(
+      "text",
+      "text-anchor"       => "middle",
+      "dominant-baseline" => "central")
+
+    regional_ids = GeoDelta::Region.get_delta_ids_in_region(x1, y1, x2, y2, 2)
+    regional_ids.each { |ids|
+      coordinates = GeoDelta::Geometry.get_coordinates(ids).map { |x, y| [x, -y] }
+
+      svg.polygon(
+        "points" => coordinates[1, 3].map { |x, y| "#{x},#{y}" }.join(" "),
+        "class"  => "a")
+    }
+
+    all_ids = GeoDelta::IdUtil.get_all_delta_ids(level)
+    all_ids.each { |ids|
+      coordinates = GeoDelta::Geometry.get_coordinates(ids).map { |x, y| [x, -y] }
+
+      svg.polygon(
+        "points" => coordinates[1, 3].map { |x, y| "#{x},#{y}" }.join(" "),
+        "class"  => "b")
+
+      svg.text(
+        ids.join(","),
+        "x"         => coordinates[0][0],
+        "y"         => coordinates[0][1],
+        "font-size" => "1.5")
+    }
+
+    svg.rect(
+      "x"            => x1,
+      "y"            => -y1,
+      "width"        => dx,
+      "height"       => dy,
+      "fill"         => "none",
+      "stroke"       => "red",
+      "stroke-width" => 0.1)
+
+    io = StringIO.new
+    svg.write(io)
+
+    content_type("image/svg+xml")
+    return io.string
   end
 end
