49749c87522aceb8369a0e439cd31da629228094
Yuya Kato <yuyakato@gmail.com>
2011-11-15 00:29:27 +0900

    「/demo/random_region_delta_level3.svg」を追加

--- src/ruby/example/geodelta_server/server.rb
+++ src/ruby/example/geodelta_server/server.rb
@@ -66,7 +66,7 @@ class GeoDeltaServer < Sinatra::Base
       "text-anchor"       => "middle",
       "dominant-baseline" => "central")
 
-    regional_ids = GeoDelta::Region.get_delta_ids_in_region(x1, y1, x2, y2, 2)
+    regional_ids = GeoDelta::Region.get_delta_ids_in_region(x1, y1, x2, y2, level)
     regional_ids.each { |ids|
       coordinates = GeoDelta::Geometry.get_coordinates(ids).map { |x, y| [x, -y] }
 
@@ -88,6 +88,74 @@ class GeoDeltaServer < Sinatra::Base
         "x"         => coordinates[0][0],
         "y"         => coordinates[0][1],
         "font-size" => "1.5")
+    }
+
+    svg.rect(
+      "x"            => x1,
+      "y"            => -y1,
+      "width"        => dx,
+      "height"       => dy,
+      "fill"         => "none",
+      "stroke"       => "red",
+      "stroke-width" => 0.1)
+
+    io = StringIO.new
+    svg.write(io)
+
+    content_type("image/svg+xml")
+    return io.string
+  end
+
+  get "/demo/random_region_delta_level3.svg" do
+    level = 3
+    x1, x2 = [(rand * 24) - 12, (rand * 24) - 12].sort
+    y1, y2 = [(rand * 24) - 12, (rand * 24) - 12].sort.reverse
+    dx = x2 - x1
+    dy = y1 - y2
+
+    svg = SVG.new(
+      "width"   => "190mm",
+      "height"  => "190mm",
+      "viewBox" => "-16.0 -14.0 32.0 28.0")
+
+    svg.style(
+      "polygon.a",
+      "fill"   => "green",
+      "stroke" => "none")
+
+    svg.style(
+      "polygon.b",
+      "fill"         => "none",
+      "stroke"       => "black",
+      "stroke-width" => "0.05")
+
+    svg.style(
+      "text",
+      "text-anchor"       => "middle",
+      "dominant-baseline" => "central")
+
+    regional_ids = GeoDelta::Region.get_delta_ids_in_region(x1, y1, x2, y2, level)
+    regional_ids.each { |ids|
+      coordinates = GeoDelta::Geometry.get_coordinates(ids).map { |x, y| [x, -y] }
+
+      svg.polygon(
+        "points" => coordinates[1, 3].map { |x, y| "#{x},#{y}" }.join(" "),
+        "class"  => "a")
+    }
+
+    all_ids = GeoDelta::IdUtil.get_all_delta_ids(level)
+    all_ids.each { |ids|
+      coordinates = GeoDelta::Geometry.get_coordinates(ids).map { |x, y| [x, -y] }
+
+      svg.polygon(
+        "points" => coordinates[1, 3].map { |x, y| "#{x},#{y}" }.join(" "),
+        "class"  => "b")
+
+      svg.text(
+        ids.join(","),
+        "x"         => coordinates[0][0],
+        "y"         => coordinates[0][1],
+        "font-size" => "0.75")
     }
 
     svg.rect(
