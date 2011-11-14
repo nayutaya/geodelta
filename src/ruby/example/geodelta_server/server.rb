8979a0257cb6456ad56c7ab7c87cf082f0bfda89
Yuya Kato <yuyakato@gmail.com>
2011-11-15 00:42:25 +0900

    「/demo/delta_level2.svg」を追加

--- src/ruby/example/geodelta_server/server.rb
+++ src/ruby/example/geodelta_server/server.rb
@@ -38,6 +38,32 @@ class GeoDeltaServer < Sinatra::Base
     return JSON.dump(response)
   end
 
+  get "/demo/delta_level2.svg" do
+    level = 2
+
+    svg = SVG.new("width" => "190mm", "height" => "190mm", "viewBox" => "-16.0 -14.0 32.0 28.0")
+    svg.style("polygon", "fill" => "none", "stroke" => "black", "stroke-width" => "0.05")
+    svg.style("text", "text-anchor" => "middle", "dominant-baseline" => "central")
+
+    all_ids = GeoDelta::IdUtil.get_all_delta_ids(level)
+    all_ids.each { |ids|
+      coordinates = GeoDelta::Geometry.get_coordinates(ids).map { |x, y| [x, -y] }
+
+      svg.polygon("points" => coordinates[1, 3].map { |x, y| "#{x},#{y}" }.join(" "))
+      svg.text(
+        ids.join(","),
+        "x"         => coordinates[0][0],
+        "y"         => coordinates[0][1],
+        "font-size" => "2.0")
+    }
+
+    io = StringIO.new
+    svg.write(io)
+
+    content_type("image/svg+xml")
+    return io.string
+  end
+
   get "/demo/random_region_delta_level2.svg" do
     level = 2
     x1, x2 = [(rand * 24) - 12, (rand * 24) - 12].sort
