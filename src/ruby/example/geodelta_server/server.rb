f9d1360e29ae65e6eb8b5d76f061972203f32a8d
Yuya Kato <yuyakato@gmail.com>
2011-11-15 00:57:46 +0900

    リファクタリング

--- src/ruby/example/geodelta_server/server.rb
+++ src/ruby/example/geodelta_server/server.rb
@@ -69,8 +69,16 @@ class GeoDeltaServer < Sinatra::Base
     return svg.to_s
   end
 
-  get "/demo/random_region_delta_level2.svg" do
-    level = 2
+  get "/demo/random_region_delta_level.svg" do
+    level = (params["level"] || "1").to_i
+    font_size =
+      case level
+      when 2 then 2.0
+      when 3 then 0.75
+      when 4 then 0.25
+      else raise("invalid level")
+      end
+
     x1, x2 = [(rand * 24) - 12, (rand * 24) - 12].sort
     y1, y2 = [(rand * 24) - 12, (rand * 24) - 12].sort.reverse
     dx = x2 - x1
@@ -101,103 +109,7 @@ class GeoDeltaServer < Sinatra::Base
         ids.join(","),
         "x"         => coordinates[0][0],
         "y"         => coordinates[0][1],
-        "font-size" => "1.5")
-    }
-
-    svg.rect(
-      "x"            => x1,
-      "y"            => -y1,
-      "width"        => dx,
-      "height"       => dy,
-      "fill"         => "none",
-      "stroke"       => "red",
-      "stroke-width" => 0.1)
-
-    content_type(svg.mime_type)
-    return svg.to_s
-  end
-
-  get "/demo/random_region_delta_level3.svg" do
-    level = 3
-    x1, x2 = [(rand * 24) - 12, (rand * 24) - 12].sort
-    y1, y2 = [(rand * 24) - 12, (rand * 24) - 12].sort.reverse
-    dx = x2 - x1
-    dy = y1 - y2
-
-    svg = SVG.new("width" => "190mm", "height" => "190mm", "viewBox" => "-16.0 -14.0 32.0 28.0")
-    svg.style("polygon.a", "fill" => "green", "stroke" => "none")
-    svg.style("polygon.b", "fill" => "none", "stroke" => "black", "stroke-width" => "0.05")
-    svg.style("text", "text-anchor" => "middle", "dominant-baseline" => "central")
-
-    regional_ids = GeoDelta::Region.get_delta_ids_in_region(x1, y1, x2, y2, level)
-    regional_ids.each { |ids|
-      coordinates = GeoDelta::Geometry.get_coordinates(ids).map { |x, y| [x, -y] }
-
-      svg.polygon(
-        "points" => coordinates[1, 3].map { |x, y| "#{x},#{y}" }.join(" "),
-        "class"  => "a")
-    }
-
-    all_ids = GeoDelta::IdUtil.get_all_delta_ids(level)
-    all_ids.each { |ids|
-      coordinates = GeoDelta::Geometry.get_coordinates(ids).map { |x, y| [x, -y] }
-
-      svg.polygon(
-        "points" => coordinates[1, 3].map { |x, y| "#{x},#{y}" }.join(" "),
-        "class"  => "b")
-      svg.text(
-        ids.join(","),
-        "x"         => coordinates[0][0],
-        "y"         => coordinates[0][1],
-        "font-size" => "0.75")
-    }
-
-    svg.rect(
-      "x"            => x1,
-      "y"            => -y1,
-      "width"        => dx,
-      "height"       => dy,
-      "fill"         => "none",
-      "stroke"       => "red",
-      "stroke-width" => 0.1)
-
-    content_type(svg.mime_type)
-    return svg.to_s
-  end
-
-  get "/demo/random_region_delta_level4.svg" do
-    level = 4
-    x1, x2 = [(rand * 24) - 12, (rand * 24) - 12].sort
-    y1, y2 = [(rand * 24) - 12, (rand * 24) - 12].sort.reverse
-    dx = x2 - x1
-    dy = y1 - y2
-
-    svg = SVG.new("width" => "190mm", "height" => "190mm", "viewBox" => "-16.0 -14.0 32.0 28.0")
-    svg.style("polygon.a", "fill" => "green", "stroke" => "none")
-    svg.style("polygon.b", "fill" => "none", "stroke" => "black", "stroke-width" => "0.05")
-    svg.style("text", "text-anchor" => "middle", "dominant-baseline" => "central")
-
-    regional_ids = GeoDelta::Region.get_delta_ids_in_region(x1, y1, x2, y2, level)
-    regional_ids.each { |ids|
-      coordinates = GeoDelta::Geometry.get_coordinates(ids).map { |x, y| [x, -y] }
-
-      svg.polygon(
-        "points" => coordinates[1, 3].map { |x, y| "#{x},#{y}" }.join(" "),
-        "class"  => "a")
-    }
-
-    all_ids = GeoDelta::IdUtil.get_all_delta_ids(level)
-    all_ids.each { |ids|
-      coordinates = GeoDelta::Geometry.get_coordinates(ids).map { |x, y| [x, -y] }
-
-      svg.polygon(
-        "points" => coordinates[1, 3].map { |x, y| "#{x},#{y}" }.join(" "),
-        "class"  => "b")
-      svg.text(
-        ids.join(","),
-        "x"         => coordinates[0][0],
-        "y"         => coordinates[0][1],
-        "font-size" => "0.25")
+        "font-size" => font_size)
     }
 
     svg.rect(
