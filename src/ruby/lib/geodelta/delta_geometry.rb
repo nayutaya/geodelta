bdbd54ca48e378764392c7d062d3ae10759ebb81
Yuya Kato <yuyakato@gmail.com>
2011-11-16 10:34:06 +0900

    DeltaGeometryモジュールを追加

---
+++ src/ruby/lib/geodelta/delta_geometry.rb
@@ -0,0 +1,6 @@
+# encoding: utf-8
+
+module GeoDelta
+  module DeltaGeometry
+  end
+end
---
+++ src/ruby/test/delta_geometry_test.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+require "test/unit"
+require_relative "../lib/geodelta/delta_geometry"
+
+class GeoDeltaDeltaGeometryTest < Test::Unit::TestCase
+  def setup
+    @mod = GeoDelta::DeltaGeometry
+  end
+end
