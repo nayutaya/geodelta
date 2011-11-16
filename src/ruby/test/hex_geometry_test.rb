77958d6f73cd0351b5636282a41f45660ba82047
Yuya Kato <yuyakato@gmail.com>
2011-11-16 10:32:56 +0900

    HexGeometryモジュールを追加

---
+++ src/ruby/test/hex_geometry_test.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+require "test/unit"
+require_relative "../lib/geodelta/hex_geometry"
+
+class GeoDeltaHexGeometryTest < Test::Unit::TestCase
+  def setup
+    @mod = GeoDelta::HexGeometry
+  end
+end
