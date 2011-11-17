aba68fc769566c6b098c5ad9c1cef4ecee87d93e
Yuya Kato <yuyakato@gmail.com>
2011-11-17 23:20:17 +0900

    GeoDelta::HexRegionモジュールを追加

---
+++ src/ruby/test/hex_region_test.rb
@@ -0,0 +1,10 @@
+# encoding: utf-8
+
+require "test/unit"
+require_relative "../lib/geodelta/hex_region"
+
+class GeoDeltaHexRegionTest < Test::Unit::TestCase
+  def setup
+    @mod = GeoDelta::HexRegion
+  end
+end
