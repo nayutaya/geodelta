149d19d019906bd9ea65c8bece92b2815dcc41ff
Yuya Kato <yuyakato@gmail.com>
2011-11-23 13:29:22 +0900

    リファクタリング

--- src/ruby/lib/geodelta/core.rb
+++ src/ruby/lib/geodelta/core.rb
@@ -6,44 +6,28 @@ require_relative "encoder"
 
 module GeoDelta
   def self.get_delta_ids(lat, lng, level)
-    mx  = GeoDelta::Projector.lng_to_mx(lng)
-    my  = GeoDelta::Projector.lat_to_my(lat)
-    nx  = GeoDelta::Projector.mx_to_nx(mx)
-    ny  = GeoDelta::Projector.my_to_ny(my)
-    ids = GeoDelta::DeltaGeometry.get_delta_ids(nx, ny, level)
-    return ids
+    nx, ny = GeoDelta::Projector.latlng_to_nxy(lat, lng)
+    return GeoDelta::DeltaGeometry.get_delta_ids(nx, ny, level)
   end
 
   def self.get_delta_code(lat, lng, level)
-    ids  = self.get_delta_ids(lat, lng, level)
-    code = GeoDelta::Encoder.encode(ids)
-    return code
+    ids = self.get_delta_ids(lat, lng, level)
+    return GeoDelta::Encoder.encode(ids)
   end
 
   def self.get_center_from_delta_ids(ids)
     nx, ny = GeoDelta::DeltaGeometry.get_center(ids)
-    mx  = GeoDelta::Projector.nx_to_mx(nx)
-    my  = GeoDelta::Projector.ny_to_my(ny)
-    lng = GeoDelta::Projector.mx_to_lng(mx)
-    lat = GeoDelta::Projector.my_to_lat(my)
-    return [lat, lng]
+    return GeoDelta::Projector.nxy_to_latlng(nx, ny)
   end
 
   def self.get_center_from_delta_code(code)
-    ids    = GeoDelta::Encoder.decode(code)
-    latlng = self.get_center_from_delta_ids(ids)
-    return latlng
+    ids = GeoDelta::Encoder.decode(code)
+    return self.get_center_from_delta_ids(ids)
   end
 
   def self.get_coordinates_from_ids(ids)
-    coordinates = GeoDelta::DeltaGeometry.get_coordinates(ids)
-    return coordinates.map { |nx, ny|
-      mx  = GeoDelta::Projector.nx_to_mx(nx)
-      my  = GeoDelta::Projector.ny_to_my(ny)
-      lng = GeoDelta::Projector.mx_to_lng(mx)
-      lat = GeoDelta::Projector.my_to_lat(my)
-      [lat, lng]
-    }
+    return GeoDelta::DeltaGeometry.get_coordinates(ids).
+      map { |nx, ny| GeoDelta::Projector.nxy_to_latlng(nx, ny) }
   end
 
   def self.get_coordinates_from_code(code)
