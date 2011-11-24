ad6063f9f9f4e2198a4da252a2bf6d61d45fe6f5
Yuya Kato <yuyakato@gmail.com>
2011-11-24 22:38:12 +0900

    コメントを追加

--- src/java/geodelta/src/jp/nayutaya/geodelta/Utility.java
+++ src/java/geodelta/src/jp/nayutaya/geodelta/Utility.java
@@ -8,6 +8,14 @@ public class Utility
         // nop
     }
 
+    /**
+     * 剰余を計算する。
+     * RubyのFloat#%メソッドと同様の値を返す。
+     *
+     * @param a 剰余を計算する値
+     * @param b 法とする値
+     * @return 剰余
+     */
     public static double mod(final double a, final double b)
     {
         double x = a;
