b54956531e55f1f86dbe30d6b0f55d9aacfa12e7
Yuya Kato <yuyakato@gmail.com>
2011-11-24 22:43:17 +0900

    ワールドデルタIDをパックする処理を追加

--- src/java/geodelta-test/src/jp/nayutaya/geodelta/Packer32Test.java
+++ src/java/geodelta-test/src/jp/nayutaya/geodelta/Packer32Test.java
@@ -1,7 +1,22 @@
 
 package jp.nayutaya.geodelta;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class Packer32Test
 {
+    @Test
+    public void packWorldDelta()
+    {
+        assertEquals(0x00000000, Packer32.packWorldDelta(0));
+        assertEquals(0x10000000, Packer32.packWorldDelta(1));
+        assertEquals(0x20000000, Packer32.packWorldDelta(2));
+        assertEquals(0x30000000, Packer32.packWorldDelta(3));
+        assertEquals(0x40000000, Packer32.packWorldDelta(4));
+        assertEquals(0x50000000, Packer32.packWorldDelta(5));
+        assertEquals(0x60000000, Packer32.packWorldDelta(6));
+        assertEquals(0x70000000, Packer32.packWorldDelta(7));
+    }
 
 }
--- src/java/geodelta/src/jp/nayutaya/geodelta/Packer32.java
+++ src/java/geodelta/src/jp/nayutaya/geodelta/Packer32.java
@@ -3,5 +3,8 @@ package jp.nayutaya.geodelta;
 
 public class Packer32
 {
-
+    /* package */static int packWorldDelta(final int id)
+    {
+        return id << 28;
+    }
 }
