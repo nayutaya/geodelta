782c64372e9c73023d5d0caf886beeeb93ff2787
Yuya Kato <yuyakato@gmail.com>
2011-11-24 22:51:11 +0900

    ワールドデルタIDをパックする処理を追加

--- src/java/geodelta-test/src/jp/nayutaya/geodelta/Packer64Test.java
+++ src/java/geodelta-test/src/jp/nayutaya/geodelta/Packer64Test.java
@@ -1,7 +1,21 @@
 
 package jp.nayutaya.geodelta;
 
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
 public class Packer64Test
 {
-
+    @Test
+    public void packWorldDelta()
+    {
+        assertEquals(0x0000000000000000L, Packer64.packWorldDelta((byte)0));
+        assertEquals(0x0800000000000000L, Packer64.packWorldDelta((byte)1));
+        assertEquals(0x1000000000000000L, Packer64.packWorldDelta((byte)2));
+        assertEquals(0x1800000000000000L, Packer64.packWorldDelta((byte)3));
+        assertEquals(0x2000000000000000L, Packer64.packWorldDelta((byte)4));
+        assertEquals(0x2800000000000000L, Packer64.packWorldDelta((byte)5));
+        assertEquals(0x3000000000000000L, Packer64.packWorldDelta((byte)6));
+        assertEquals(0x3800000000000000L, Packer64.packWorldDelta((byte)7));
+    }
 }
--- src/java/geodelta/src/jp/nayutaya/geodelta/Packer64.java
+++ src/java/geodelta/src/jp/nayutaya/geodelta/Packer64.java
@@ -3,5 +3,8 @@ package jp.nayutaya.geodelta;
 
 public class Packer64
 {
-
+    /* package */static long packWorldDelta(final byte id)
+    {
+        return (long)id << 59;
+    }
 }
