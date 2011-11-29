91769be582381cdbdc1708eece7e0dacce7d4d85
Yuya Kato <yuyakato@gmail.com>
2011-11-29 21:44:23 +0900

    コメントを追加

--- src/java/geodelta/src/jp/nayutaya/geodelta/Packer32.java
+++ src/java/geodelta/src/jp/nayutaya/geodelta/Packer32.java
@@ -1,38 +1,85 @@
 
 package jp.nayutaya.geodelta;
 
+/**
+ * 32ビット符号付き整数にパックされたデルタID列を扱うクラス。
+ */
 public class Packer32
 {
+    /**
+     * ワールドデルタIDをパックする。
+     *
+     * @param id ワールドデルタID
+     * @return パックされたID
+     */
     /* package */static int packWorldDelta(final int id)
     {
         return id << 28;
     }
 
+    /**
+     * ワールドデルタIDをアンパックする。
+     *
+     * @param value パックされたデルタID列
+     * @return ワールドデルタID
+     */
     /* package */static byte unpackWorldDelta(final int value)
     {
         return (byte)((value >> 28) & 0x07);
     }
 
+    /**
+     * サブデルタIDをパックする。
+     *
+     * @param level サブデルタのレベル
+     * @param id サブデルタID
+     * @return パックされたID
+     */
     /* package */static int packSubDelta(final int level, final int id)
     {
         return id << (26 - ((level - 2) * 2));
     }
 
+    /**
+     * サブデルタIDをアンパックする。
+     *
+     * @param level サブデルタのレベル
+     * @param value パックされたデルタID列
+     * @return サブデルタID
+     */
     /* package */static byte unpackSubDelta(final int level, final int value)
     {
         return (byte)((value >> (26 - ((level - 2) * 2))) & 0x03);
     }
 
+    /**
+     * レベルをパックする。
+     *
+     * @param level レベル
+     * @return パックされたID
+     */
     /* package */static int packLevel(final int level)
     {
         return level;
     }
 
+    /**
+     * レベルをアンパックする。
+     *
+     * @param value パックされたデルタID列
+     * @return レベル
+     */
     /* package */static int unpackLevel(final int value)
     {
         return value & 0x0F;
     }
 
+    /**
+     * デルタID列を32ビット整数にパックする。
+     *
+     * @param ids デルタID列
+     * @return パックされたデルタID列
+     */
     public static int pack(final byte[] ids)
     {
         final int wid = packWorldDelta(ids[0]);
@@ -45,6 +92,12 @@ public class Packer32
         return wid | sid | level;
     }
 
+    /**
+     * 32ビット整数からデルタID列をアンパックする。
+     *
+     * @param value パックされたデルタID列
+     * @return デルタID列
+     */
     public static byte[] unpack(final int value)
     {
         final int level = unpackLevel(value);
--- src/java/geodelta/src/jp/nayutaya/geodelta/Packer64.java
+++ src/java/geodelta/src/jp/nayutaya/geodelta/Packer64.java
@@ -1,38 +1,85 @@
 
 package jp.nayutaya.geodelta;
 
+/**
+ * 64ビット符号付き整数にパックされたデルタID列を扱うクラス。
+ */
 public class Packer64
 {
+    /**
+     * ワールドデルタIDをパックする。
+     *
+     * @param id ワールドデルタID
+     * @return パックされたID
+     */
     /* package */static long packWorldDelta(final byte id)
     {
         return (long)id << 59;
     }
 
+    /**
+     * ワールドデルタIDをアンパックする。
+     *
+     * @param value パックされたデルタID列
+     * @return ワールドデルタID
+     */
     /* package */static byte unpackWorldDelta(final long value)
     {
         return (byte)((value >> 59) & 0x07);
     }
 
+    /**
+     * サブデルタIDをパックする。
+     *
+     * @param level サブデルタのレベル
+     * @param id サブデルタID
+     * @return パックされたID
+     */
     /* package */static long packSubDelta(final int level, final byte id)
     {
         return (long)id << (57 - ((level - 2) * 2));
     }
 
+    /**
+     * サブデルタIDをアンパックする。
+     *
+     * @param level サブデルタのレベル
+     * @param value パックされたデルタID列
+     * @return サブデルタID
+     */
     /* package */static byte unpackSubDelta(final int level, final long value)
     {
         return (byte)((value >> (57 - ((level - 2) * 2))) & 0x03);
     }
 
+    /**
+     * レベルをパックする。
+     *
+     * @param level レベル
+     * @return パックされたID
+     */
     /* package */static long packLevel(final int level)
     {
         return level;
     }
 
+    /**
+     * レベルをアンパックする。
+     *
+     * @param value パックされたデルタID列
+     * @return レベル
+     */
     /* package */static int unpackLevel(final long value)
     {
         return (int)(value & 0x1F);
     }
 
+    /**
+     * デルタID列を64ビット整数にパックする。
+     *
+     * @param ids デルタID列
+     * @return パックされたデルタID列
+     */
     public static long pack(final byte[] ids)
     {
         final long wid = packWorldDelta(ids[0]);
@@ -45,6 +92,12 @@ public class Packer64
         return wid | sid | level;
     }
 
+    /**
+     * 64ビット整数からデルタID列をアンパックする。
+     *
+     * @param value パックされたデルタID列
+     * @return デルタID列
+     */
     public static byte[] unpack(final long value)
     {
         final int level = unpackLevel(value);
