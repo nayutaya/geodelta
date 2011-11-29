00025ea79bbe84f803032452507f78bc029e529e
Yuya Kato <yuyakato@gmail.com>
2011-11-29 22:11:53 +0900

    テストデータでクロステストするテストケースを追加

---
+++ src/java/geodelta-test/src/jp/nayutaya/geodelta/CrossTest.java
@@ -0,0 +1,48 @@
+
+package jp.nayutaya.geodelta;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+import org.junit.Test;
+
+public class CrossTest
+{
+    @Test
+    public void center() throws IOException
+    {
+        final FileReader fr = new FileReader("../../../data/delta_center.txt");
+        final BufferedReader br = new BufferedReader(fr);
+
+        String line = null;
+        int lineNo = 0;
+        while ( (line = br.readLine()) != null )
+        {
+            lineNo++;
+            if ( lineNo == 1 )
+            {
+                continue;
+            }
+
+            final String[] tuple = line.split("\\|");
+            final double randomLat = Double.parseDouble(tuple[0].trim());
+            final double randomLng = Double.parseDouble(tuple[1].trim());
+            final int level = Integer.parseInt(tuple[2].trim());
+            final String code = tuple[3].trim();
+            final double centerLat = Double.parseDouble(tuple[4].trim());
+            final double centerLng = Double.parseDouble(tuple[5].trim());
+
+            final String ref = "line:" + lineNo + " level:" + level;
+            final String code2 = GeoDelta.getDeltaCode(randomLat, randomLng, level);
+            final double[] center2 = GeoDelta.getCenter(code2);
+            assertEquals(ref, code, code2);
+            assertEquals(ref, centerLat, center2[0], 1.0E-13);
+            assertEquals(ref, centerLng, center2[1], 1.0E-15);
+        }
+
+        fr.close();
+    }
+}
