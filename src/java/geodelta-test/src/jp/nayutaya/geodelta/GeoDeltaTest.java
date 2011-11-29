773956c5fba0e28914ce1af787222398c08dfb2f
Yuya Kato <yuyakato@gmail.com>
2011-11-29 21:52:44 +0900

    緯度経度からデルタID列、GeoDeltaコードを取得する処理を追加

--- src/java/geodelta-test/src/jp/nayutaya/geodelta/GeoDeltaTest.java
+++ src/java/geodelta-test/src/jp/nayutaya/geodelta/GeoDeltaTest.java
@@ -1,7 +1,50 @@
 
 package jp.nayutaya.geodelta;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 public class GeoDeltaTest
 {
+    @Test
+    public void getDeltaIds()
+    {
+        assertArrayEquals(new byte[] {0}, GeoDelta.getDeltaIds(+45.0, +0.0, 1));
+        assertArrayEquals(new byte[] {1}, GeoDelta.getDeltaIds(+45.0, +90.0, 1));
+        assertArrayEquals(new byte[] {2}, GeoDelta.getDeltaIds(+45.0, +180.0, 1));
+        assertArrayEquals(new byte[] {3}, GeoDelta.getDeltaIds(+45.0, -90.0, 1));
+        assertArrayEquals(new byte[] {2}, GeoDelta.getDeltaIds(+45.0, -180.0, 1));
 
+        assertArrayEquals(new byte[] {4}, GeoDelta.getDeltaIds(-45.0, +0.0, 1));
+        assertArrayEquals(new byte[] {5}, GeoDelta.getDeltaIds(-45.0, +90.0, 1));
+        assertArrayEquals(new byte[] {6}, GeoDelta.getDeltaIds(-45.0, +180.0, 1));
+        assertArrayEquals(new byte[] {7}, GeoDelta.getDeltaIds(-45.0, -90.0, 1));
+        assertArrayEquals(new byte[] {6}, GeoDelta.getDeltaIds(-45.0, -180.0, 1));
+
+        assertArrayEquals(new byte[] {0}, GeoDelta.getDeltaIds(+0.0, +0.0, 1));
+        assertArrayEquals(new byte[] {0, 1}, GeoDelta.getDeltaIds(+0.0, +0.0, 2));
+        assertArrayEquals(new byte[] {0, 1, 1}, GeoDelta.getDeltaIds(+0.0, +0.0, 3));
+        assertArrayEquals(new byte[] {0, 1, 1, 1}, GeoDelta.getDeltaIds(+0.0, +0.0, 4));
+    }
+
+    @Test
+    public void getDeltaCode()
+    {
+        assertEquals("Z", GeoDelta.getDeltaCode(+45.0, +0.0, 1));
+        assertEquals("Y", GeoDelta.getDeltaCode(+45.0, +90.0, 1));
+        assertEquals("X", GeoDelta.getDeltaCode(+45.0, +180.0, 1));
+        assertEquals("W", GeoDelta.getDeltaCode(+45.0, -90.0, 1));
+        assertEquals("X", GeoDelta.getDeltaCode(+45.0, -180.0, 1));
+
+        assertEquals("V", GeoDelta.getDeltaCode(-45.0, +0.0, 1));
+        assertEquals("T", GeoDelta.getDeltaCode(-45.0, +90.0, 1));
+        assertEquals("S", GeoDelta.getDeltaCode(-45.0, +180.0, 1));
+        assertEquals("R", GeoDelta.getDeltaCode(-45.0, -90.0, 1));
+        assertEquals("S", GeoDelta.getDeltaCode(-45.0, -180.0, 1));
+
+        assertEquals("Z", GeoDelta.getDeltaCode(+0.0, +0.0, 1));
+        assertEquals("ZM", GeoDelta.getDeltaCode(+0.0, +0.0, 2));
+        assertEquals("Z7", GeoDelta.getDeltaCode(+0.0, +0.0, 3));
+        assertEquals("Z7M", GeoDelta.getDeltaCode(+0.0, +0.0, 4));
+    }
 }
--- src/java/geodelta/src/jp/nayutaya/geodelta/GeoDelta.java
+++ src/java/geodelta/src/jp/nayutaya/geodelta/GeoDelta.java
@@ -1,7 +1,36 @@
 
 package jp.nayutaya.geodelta;
 
+/**
+ * 主要な機能を提供するクラス。
+ */
 public class GeoDelta
 {
+    /**
+     * 緯度経度からデルタID列を取得する。
+     *
+     * @param lat 緯度
+     * @param lng 経度
+     * @param level レベル（1～）
+     * @return デルタID列
+     */
+    public static byte[] getDeltaIds(final double lat, final double lng, final int level)
+    {
+        final double[] nxy = Projector.latLngToNxy(lat, lng);
+        return Geometry.getDeltaIds(nxy[0], nxy[1], level);
+    }
 
+    /**
+     * 緯度経度からGeoDeltaコードを取得する。
+     *
+     * @param lat 緯度
+     * @param lng 経度
+     * @param level レベル（1～）
+     * @return GeoDeltaコード
+     */
+    public static String getDeltaCode(final double lat, final double lng, final int level)
+    {
+        final byte[] ids = GeoDelta.getDeltaIds(lat, lng, level);
+        return Encoder.encode(ids);
+    }
 }
