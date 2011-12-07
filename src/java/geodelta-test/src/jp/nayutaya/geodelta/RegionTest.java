5ab894df51a87da3685aa14dc37cb92ee09f7636
Yuya Kato <yuyakato@gmail.com>
2011-12-08 01:44:32 +0900

    領域内のデルタを取得する処理を追加

--- src/java/geodelta-test/src/jp/nayutaya/geodelta/RegionTest.java
+++ src/java/geodelta-test/src/jp/nayutaya/geodelta/RegionTest.java
@@ -1,7 +1,164 @@
 
 package jp.nayutaya.geodelta;
 
+import static org.junit.Assert.assertArrayEquals;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class RegionTest
 {
+    @Test
+    public void getDeltaIdsInRegion_level1()
+    {
+        // @formatter:off
+        assertArrayArrayEquals(
+            new byte[][] {{0}},
+            Region.getDeltaIdsInRegion(+0.0, +8.0, +0.0, +8.0, 1));
+        assertArrayArrayEquals(
+            new byte[][] {{1}},
+            Region.getDeltaIdsInRegion(+6.0, +4.0, +6.0, +4.0, 1));
 
+        assertArrayArrayEquals(
+            new byte[][] {{0}, {1}},
+            Region.getDeltaIdsInRegion(+1.0, +6.0, +5.0, +6.0, 1));
+        assertArrayArrayEquals(
+            new byte[][] {{4}, {5}},
+            Region.getDeltaIdsInRegion(+1.0, -6.0, +5.0, -6.0, 1));
+        assertArrayArrayEquals(
+            new byte[][] {{0}, {4}},
+            Region.getDeltaIdsInRegion(+0.0, +5.0, +0.0, -5.0, 1));
+        assertArrayArrayEquals(
+            new byte[][] {{1}, {5}},
+            Region.getDeltaIdsInRegion(+6.0, +5.0, +6.0, -5.0, 1));
+
+        assertArrayArrayEquals(
+            new byte[][] {{0}, {1}, {2}},
+            Region.getDeltaIdsInRegion(+1.0, +6.0, +11.0, +6.0, 1));
+        assertArrayArrayEquals(
+            new byte[][] {{4}, {5}, {6}},
+            Region.getDeltaIdsInRegion(+1.0, -6.0, +11.0, -6.0, 1));
+
+        assertArrayArrayEquals(
+            new byte[][] {{0}, {1}, {4}, {5}},
+            Region.getDeltaIdsInRegion(+1.0, +5.0, +5.0, -5.0, 1));
+        // @formatter:on
+    }
+
+    @Test
+    public void getDeltaIdsInRegion_level1_roundtrip()
+    {
+        // @formatter:off
+        assertArrayArrayEquals(
+            new byte[][] {{0}, {1}, {3}},
+            Region.getDeltaIdsInRegion(-6.0, +6.0, +6.0, +6.0, 1));
+        assertArrayArrayEquals(
+            new byte[][] {{0}, {2}, {3}},
+            Region.getDeltaIdsInRegion(+12.0, +6.0, -6.0, +6.0, 1));
+        // @formatter:on
+    }
+
+    @Test
+    public void getDeltaIdsInRegion_level2()
+    {
+        // @formatter:off
+        assertArrayArrayEquals(
+            new byte[][] {{0, 1}, {1, 3}, {3, 2}, {4, 1}, {5, 2}, {7, 3}},
+            Region.getDeltaIdsInRegion(-1.0, +4.0, +1.0, -4.0, 2));
+        assertArrayArrayEquals(
+            new byte[][] {{0, 1}, {1, 3}, {3, 2}, {4, 1}, {5, 2}, {7, 3}},
+            Region.getDeltaIdsInRegion(-4.0, +1.0, +4.0, -1.0, 2));
+        assertArrayArrayEquals(
+            new byte[][] {{0, 0}, {0, 1}, {0, 2}, {0, 3}, {1, 3}, {3, 2}, {4, 0}, {4, 1}, {4, 2}, {4, 3}, {5, 2}, {7, 3}},
+            Region.getDeltaIdsInRegion(-3.0, +9.0, +3.0, -9.0, 2));
+        // @formatter:on
+    }
+
+    @Test
+    public void getDeltaIdsInRegion_level3()
+    {
+        {
+            // @formatter:off
+            final byte[][] expecteds = {
+                {0, 1, 1},
+                {1, 3, 3},
+                {3, 2, 2},
+                {4, 1, 1},
+                {5, 2, 2},
+                {7, 3, 3},
+            };
+            // @formatter:on
+            assertArrayArrayEquals(expecteds, Region.getDeltaIdsInRegion(-1.5, +1.5, +1.5, -1.5, 3));
+        }
+        {
+            // @formatter:off
+            final byte[][] expecteds = {
+                {2, 1, 1},
+                {3, 3, 0},
+                {3, 3, 3},
+                {6, 1, 1},
+                {7, 2, 0},
+                {7, 2, 2},
+            };
+            // @formatter:on
+            assertArrayArrayEquals(expecteds, Region.getDeltaIdsInRegion(-11.5, +1.5, -9.5, -1.5, 3));
+        }
+    }
+
+    // TODO: test_getDeltaIdsInRegion__random
+    // def test_getDeltaIdsInRegion__random
+    // 10.times {
+    // (3..5).each { |level|
+    // x1, x2 = [(rand * 24) - 12, (rand * 24) - 12].sort
+    // y1, y2 = [(rand * 24) - 12, (rand * 24) - 12].sort.reverse
+    // regional_ids = GeoDelta::Region.getDeltaIdsInRegion(x1, y1, x2, y2, level)
+    // 10.times {
+    // cx, cy = [x1 + (rand * (x2 - x1)), y1 - (rand * (y1 - y2))]
+    // test_ids = GeoDelta::DeltaGeometry.get_delta_ids(cx, cy, level)
+    // assert_equal(
+    // {:rect => [[x1, y1], [x2, y2]], :test_ids => test_ids, :include => true},
+    // {:rect => [[x1, y1], [x2, y2]], :test_ids => test_ids, :include => regional_ids.include?(test_ids)})
+    // }
+    // }
+    // }
+    // end
+
+    private void assertArrayArrayEquals(final byte[][] expecteds, final byte[][] actuals)
+    {
+        assertEquals(expecteds.length, actuals.length);
+        for ( int i = 0, len = expecteds.length; i < len; i++ )
+        {
+            assertArrayEquals(expecteds[i], actuals[i]);
+        }
+    }
+
+    @SuppressWarnings("unused")
+    private void assertArrayArrayEqualsWithPrint(final byte[][] expecteds, final byte[][] actuals)
+    {
+        System.out.print("expecteds {");
+        for ( int i = 0; i < expecteds.length; i++ )
+        {
+            System.out.print(" {");
+            for ( int j = 0; j < expecteds[i].length; j++ )
+            {
+                System.out.print(" " + expecteds[i][j]);
+            }
+            System.out.print(" }");
+        }
+        System.out.println(" }");
+
+        System.out.print("actuals   {");
+        for ( int i = 0; i < actuals.length; i++ )
+        {
+            System.out.print(" {");
+            for ( int j = 0; j < actuals[i].length; j++ )
+            {
+                System.out.print(" " + actuals[i][j]);
+            }
+            System.out.print(" }");
+        }
+        System.out.println(" }");
+
+        assertArrayArrayEquals(expecteds, actuals);
+    }
 }
--- src/java/geodelta/src/jp/nayutaya/geodelta/Region.java
+++ src/java/geodelta/src/jp/nayutaya/geodelta/Region.java
@@ -1,7 +1,135 @@
 
 package jp.nayutaya.geodelta;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Region
 {
+    public static byte[][] getDeltaIdsInRegion(final double x1, final double y1, final double x2, final double y2, final int level)
+    {
+        Set<Long> packedIdsSet = null;
+        if ( x1 > x2 )
+        {
+            packedIdsSet = new HashSet<Long>();
+            packedIdsSet.addAll(getDeltaIdsInRegionSet(x1, y1, +12.0, y2, level));
+            packedIdsSet.addAll(getDeltaIdsInRegionSet(x2, y1, +0.0, y2, level));
+        }
+        else
+        {
+            packedIdsSet = getDeltaIdsInRegionSet(x1, y1, x2, y2, level);
+        }
 
+        final Long[] packedIdsArray = packedIdsSet.toArray(new Long[0]);
+        Arrays.sort(packedIdsArray);
+        final byte[][] result = new byte[packedIdsArray.length][];
+        for ( int i = 0, len = packedIdsArray.length; i < len; i++ )
+        {
+            result[i] = Packer64.unpack(packedIdsArray[i]);
+        }
+
+        return result;
+    }
+
+    private static Set<Long> getDeltaIdsInRegionSet(final double x1, final double y1, final double x2, final double y2, final int level)
+    {
+        final Set<Long> packedIdsSet = new HashSet<Long>();
+
+        final double unit = 12.0 / Math.pow(2, level - 1);
+        final double u1 = unit;
+        final double u2 = unit / 2.0;
+
+        final byte[] nw = Geometry.getDeltaIds(x1, y1, level);
+        final byte[] ne = Geometry.getDeltaIds(x2, y1, level);
+        final byte[] sw = Geometry.getDeltaIds(x1, y2, level);
+        final byte[] se = Geometry.getDeltaIds(x2, y2, level);
+
+        {
+            final double[] nwxy = Geometry.getCenter(nw);
+            final double[] nexy = Geometry.getCenter(ne);
+            double sx = nwxy[0];
+            double ex = nexy[0];
+            final double y = nwxy[1];
+
+            if ( sx > ex )
+            {
+                sx -= 24.0;
+            }
+
+            int sxi = (int)Math.floor(sx / u2);
+            int exi = (int)Math.ceil(ex / u2);
+
+            if ( !Geometry.isUpperDelta(nw) && x1 < sx && !Arrays.equals(nw, sw) )
+            {
+                sxi -= 1;
+            }
+            if ( !Geometry.isUpperDelta(ne) && x2 > ex && !Arrays.equals(ne, se) )
+            {
+                exi += 1;
+            }
+
+            for ( int xi = sxi; xi <= exi; xi++ )
+            {
+                final double xx = xi * u2;
+                packedIdsSet.add(getDeltaIdsAsLong(xx, y, level));
+            }
+        }
+
+        {
+            final double[] swxy = Geometry.getCenter(sw);
+            final double[] sexy = Geometry.getCenter(se);
+            double sx = swxy[0];
+            double ex = sexy[0];
+            final double y = swxy[1];
+
+            if ( sx > ex )
+            {
+                sx -= 24.0;
+            }
+
+            int sxi = (int)Math.floor(sx / u2);
+            int exi = (int)Math.ceil(ex / u2);
+
+            if ( Geometry.isUpperDelta(sw) && x1 < sx && !Arrays.equals(nw, sw) )
+            {
+                sxi -= 1;
+            }
+            if ( Geometry.isUpperDelta(se) && x2 > ex && !Arrays.equals(ne, se) )
+            {
+                exi += 1;
+            }
+
+            for ( int xi = sxi; xi <= exi; xi++ )
+            {
+                final double xx = xi * u2;
+                packedIdsSet.add(getDeltaIdsAsLong(xx, y, level));
+            }
+        }
+
+        {
+            final int syi = (int)Math.floor(y1 / u1);
+            final int eyi = (int)Math.ceil(y2 / u1) + 1;
+            final int sxi = (int)Math.floor(x1 / u2);
+            final int exi = (int)Math.ceil(x2 / u2);
+
+            for ( int yi = eyi; yi <= syi; yi++ )
+            {
+                final double yy = yi * u1 - u2;
+                for ( int xi = sxi; xi <= exi; xi++ )
+                {
+                    final double xx = xi * u2;
+                    packedIdsSet.add(getDeltaIdsAsLong(xx, yy, level));
+                }
+            }
+        }
+
+        return packedIdsSet;
+    }
+
+    private static long getDeltaIdsAsLong(final double x, final double y, final int level)
+    {
+        final byte[] ids = Geometry.getDeltaIds(x, y, level);
+        return Packer64.pack(ids);
+    }
 }
